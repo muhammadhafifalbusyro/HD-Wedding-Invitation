5fb15b06a89568ce36403102ae9bef804b3ac0d9
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2025-03-01 11:34:31 +0700

    Finish Digital Invitation

---
+++ names.js
@@ -0,0 +1,20 @@
+// names.js
+
+const allowedNames = [
+    "Abdul Karim", "Agus Setiawan", "Ahmad Yani", "Angel Hanatasya", "Anis Silaturakhmi",
+    "Casinih", "Dian", "Amalia Nurhakiki", "Ela", "Asep", "Ayum", "Ayyub",
+    "Dewi Utari Armin", "Dewi Sekaone", "Teh Dinanti", "Diyanti", "Eki Sekaone",
+    "Erni Komalla", "Teh Fitri", "Fitriyatull", "Ibu Isah", "Ibu Rina",
+    "Ike Sekaone", "Indriyani Sapitri", "Khoerunnisa", "Kiki Mae", "Lilim Sekaone",
+    "Maryati", "Nasyaa", "Nupita Sari", "Pitriyanti", "Voni Anuresta",
+    "Friska Widasera", "Rita Sekaone", "Rosana", "Rudi Yanto", "Siti Rumiani",
+    "Sadad", "Shafira", "Sri Mulyati Sekaone", "Supriyanto", "Tarih Anggraeni",
+    "Tasya Nazulla", "Danengsih", "Tika", "Luthfiyah", "Tinah Yuliana",
+    "Wiyanti", "Novita Sari", "Vicky", "Niken FM", "Tia Mae",
+    "Fajar Ms", "Nur Hafifah", "Gea Amalia", "Ina", "Ika Susanti",
+    "Shavita", "Dewi Fortuna", "Ratih", "Anita", "Rezaa",
+    "Ramaa", "Tri Ningsih", "Teh Yunita", "Tinas Supriyatin", "Adkhiyahtus",
+    "Saripah", "Sulis", "Robi Ilyas", "Ahmad Syahrul (Arab)", "Lela Puspa",
+    "Repi", "Tuniah Sari", "Bayu", "Siffa Kusuma Dewi", "Diah Oktaviani",
+    "Bu Badriyah", "Muhammad Hafif Al Busyro", "Dede Oktaviana","Syofyan Zuhad Nasik", "Haechan", "Nur Cendrawati", "Rifki Fajrian","Isabella Maharani"
+  ];
